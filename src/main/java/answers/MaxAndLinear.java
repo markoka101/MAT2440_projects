145a29b85a62934027210a150e0bbe7aa3cee78a
markoka101 <markoka101@gmail.com>
2024-10-30 16:20:47 -0400

    implemeted functions

--- src/main/java/answers/MaxAndLinear.java
+++ src/main/java/answers/MaxAndLinear.java
@@ -1,4 +1,28 @@
 package answers;
 
 public class MaxAndLinear {
+
+    public MaxAndLinear(){}
+
+    public int maxSearch(int[] nums) {
+        int max = 0;
+
+        for (int n : nums) {
+            if (n > max) {
+                max = n;
+            }
+        }
+
+        return max;
+    }
+
+    public boolean linearSearch(int target, int[] nums) {
+        for (int n : nums) {
+            if (n == target) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
