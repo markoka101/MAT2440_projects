34471656889beea153704ece0f4265669c68225a
markoka101 <markoka101@gmail.com>
2024-11-01 17:27:12 -0400

    completed class and commented on code for clarity

--- src/main/java/answers/PrimalityTesting.java
+++ src/main/java/answers/PrimalityTesting.java
@@ -1,4 +1,30 @@
 package answers;
 
 public class PrimalityTesting {
+    //constructor
+    public PrimalityTesting(){}
+
+    //returns true or false if n is prime
+    public boolean isPrime(int n) {
+        //check if n is 1
+        if (n == 1) {
+            return false;
+        }
+
+        //limit is square root of the number
+        int limit = (int)Math.sqrt(n);
+
+        //check all numbers from 2 to limit if factor
+        for (int i = 2; i <= limit; i++) {
+
+            //take mod of number and i
+            // if it's zero then it's not prime
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        //if loop ends, it's a prime number
+        return true;
+    }
 }
